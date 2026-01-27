32912e886a70ce42274f984d4cc603c8ec433b28
ALonDe35 <46168302+ALonDe35@users.noreply.github.com>
2026-01-27 16:55:35 +0300

    Update sg.py

--- sg.py
+++ sg.py
@@ -352,4 +352,23 @@ app.add_handler(CallbackQueryHandler(start, pattern="^back_to_main$"))
 app.add_handler(CallbackQueryHandler(handle_answer, pattern="^ans_"))
 
 print("Bot aktif... Sahil Güvenlik Sistemi çalışıyor.")
+
 app.run_polling()
+# --- Render için mini web server ---
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_server).start()
+# --- bitti ---
